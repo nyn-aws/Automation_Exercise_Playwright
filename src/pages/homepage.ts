d4cc4796015028f0e770ab6bf283410c8413f10f
NYN <contact.nyn2025@gmail.com>
2025-08-26 12:59:47 +0530

    feat: Refactor Playwright tests, introducing POM, data structuring, and utility abstraction

--- src/pages/homepage.ts
+++ src/pages/homepage.ts
@@ -1,4 +1,25 @@
 import { Page, Locator } from "@playwright/test";
+
+export interface DateSelection {
+  day: string;
+  month: string;
+  year: string;
+}
+
+export interface RegistrationFormData {
+  firstName: string;
+  lastName: string;
+  company: string;
+  address1: string;
+  address2: string;
+  country: string;
+  state: string;
+  city: string;
+  zipcode: string;
+  mobileNumber: string;
+  password: string; // Add password to the interface
+}
+
 export class Homepage {
   private page: Page;
   public locators: Record<string, Locator>;
@@ -11,6 +32,53 @@ export class Homepage {
   async goto(url: string = "") {
     this.page.goto("");
     this.page.waitForLoadState("networkidle");
+  }
+
+  async navigateToSignup() {
+    await this.locators.login_signup_button.click();
+    await this.page.waitForLoadState("networkidle");
+  }
+
+  async input_signup_details(name_value: string, email_value: string) {
+    await this.locators.sign_up_name_field.fill(name_value);
+    await this.locators.sign_up_email_field.fill(email_value);
+    await this.locators.sign_up_button.click();
+  }
+
+  async input_registration_form_details(
+    dateSelection: DateSelection,
+    formData: RegistrationFormData
+  ) {
+    await this.locators.radio_button_mr.check();
+    await this.locators.registration_form_password.fill(formData.password);
+    await this.locators.day_dropdown.selectOption(dateSelection.day);
+    await this.locators.month_dropdown.selectOption(dateSelection.month);
+    await this.locators.year_dropdown.selectOption(dateSelection.year);
+    await this.locators.newsletter_checkbox.check();
+    await this.locators.receive_offers_checkbox.check();
+    await this.locators.receive_offers_checkbox.uncheck();
+    await this.locators.first_name_field.fill(formData.firstName);
+    await this.locators.last_name_field.fill(formData.lastName);
+    await this.locators.company_field.fill(formData.company);
+    await this.locators.address1_field.fill(formData.address1);
+    await this.locators.address2_field.fill(formData.address2);
+    await this.locators.country_dropdown.selectOption(formData.country);
+    await this.locators.state_field.fill(formData.state);
+    await this.locators.city_field.fill(formData.city);
+    await this.locators.zipcode_field.fill(formData.zipcode);
+    await this.locators.mobile_number_field.fill(formData.mobileNumber);
+  }
+
+  async submitRegistrationForm() {
+    await this.locators.create_account_button.click();
+  }
+
+  async continueToHomepage() {
+    await this.locators.continue_button.click();
+  }
+
+  async deleteAccount() {
+    await this.locators.delete_account_button.click();
   }
 
   private initLocators(): Record<string, Locator> {
--- tests/test.spec.ts
+++ tests/test.spec.ts
@@ -1,5 +1,9 @@
 import { test, expect, Locator } from "@playwright/test";
-import { Homepage } from "../src/pages/homepage";
+import {
+  Homepage,
+  DateSelection,
+  RegistrationFormData,
+} from "../src/pages/homepage";
 import { CommonUtils } from "../src/utils/commonUtils";
 
 test.describe("Automation Exercise", () => {
@@ -9,6 +13,10 @@ test.describe("Automation Exercise", () => {
 
     homepage = new Homepage(page);
     await homepage.goto();
+    // Page Title and URL assertions
+    await expect(page).toHaveURL("https://www.automationexercise.com/");
+    await expect(page).toHaveTitle("Automation Exercise");
+    await expect(homepage.locators.website_logo).toBeVisible();
   });
   test.afterEach(async ({ page }, testInfo) => {
     // Log test title,status and time taken in one log
@@ -20,161 +28,101 @@ test.describe("Automation Exercise", () => {
   test("Test Case 1: Register User", async ({ page }) => {
     test.slow();
 
-    await expect(page).toHaveURL("https://www.automationexercise.com/");
-    await expect(page).toHaveTitle("Automation Exercise");
-    await expect(homepage.locators.website_logo).toBeVisible();
-
-    await expect(homepage.locators.login_signup_button).toBeVisible();
-    await homepage.locators.login_signup_button.click();
-    await page.waitForLoadState("networkidle");
-
-    await expect(homepage.locators.new_user_sign_up).toBeVisible();
     const name_value = "AutomationUser1";
     const email_value = CommonUtils.generateRandomEmail();
     const random_password = CommonUtils.generateRandomPassword();
     const address_data =
       "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.";
 
-    await homepage.locators.sign_up_name_field.fill(name_value);
-    await homepage.locators.sign_up_email_field.fill(email_value);
-    await homepage.locators.sign_up_button.click();
+    await homepage.navigateToSignup();
+    await expect(homepage.locators.login_signup_button).toBeVisible();
+    await expect(homepage.locators.new_user_sign_up).toBeVisible();
+    await homepage.input_signup_details(name_value, email_value);
 
+    // Assertions for Enter Account Information page
     await expect
       .soft(homepage.locators.enter_account_info_heading)
       .toBeVisible();
-
-    //  Registration Form Fields
-
-    await homepage.locators.radio_button_mr.check();
-
-    await expect(homepage.locators.radio_button_mr).toBeChecked();
-    await expect(homepage.locators.radio_button_mrs).not.toBeChecked();
-
-    await expect(homepage.locators.registration_form_name).toBeVisible();
     await expect(homepage.locators.registration_form_name).toHaveValue(
       name_value
     );
-
-    await expect(homepage.locators.registration_form_email).toBeVisible();
     await expect(homepage.locators.registration_form_email).toHaveValue(
       email_value
     );
 
-    await expect(homepage.locators.registration_form_password).toBeVisible();
-    await expect(homepage.locators.registration_form_password).toBeEnabled();
+    const dateSelection: DateSelection = {
+      day: "15",
+      month: "January",
+      year: "2000",
+    };
 
-    await homepage.locators.registration_form_password.fill(random_password);
+    const registrationFormData: RegistrationFormData = {
+      firstName: "Automation",
+      lastName: "Automation",
+      company: "Automation",
+      address1: address_data,
+      address2: address_data,
+      country: "United States",
+      state: "California",
+      city: "Los Angeles",
+      zipcode: "90001",
+      mobileNumber: "1234567890",
+      password: random_password,
+    };
+
+    await homepage.input_registration_form_details(
+      dateSelection,
+      registrationFormData
+    );
+
+    // Assertions for registration form inputs
+    await expect(homepage.locators.radio_button_mr).toBeChecked();
+    await expect(homepage.locators.radio_button_mrs).not.toBeChecked();
     await expect(homepage.locators.registration_form_password).toHaveValue(
       random_password
     );
-
-    // date selection dropdowns
-
-    await expect(homepage.locators.day_dropdown).toBeVisible();
-    await expect(homepage.locators.month_dropdown).toBeVisible();
-    await expect(homepage.locators.year_dropdown).toBeVisible();
-
-    await homepage.locators.day_dropdown.selectOption("15");
-    await homepage.locators.month_dropdown.selectOption("January");
-    await homepage.locators.year_dropdown.selectOption("2000");
-
-    await expect(homepage.locators.newsletter_checkbox).toBeVisible();
-    await expect(homepage.locators.newsletter_checkbox).toBeEnabled();
-    await homepage.locators.newsletter_checkbox.check();
+    await expect(homepage.locators.day_dropdown).toHaveValue("15");
+    await expect(homepage.locators.month_dropdown).toHaveValue("1"); // Month 'January' has value '1'
+    await expect(homepage.locators.year_dropdown).toHaveValue("2000");
     await expect(homepage.locators.newsletter_checkbox).toBeChecked();
-
-    await expect(homepage.locators.receive_offers_checkbox).toBeVisible();
-    await expect(homepage.locators.receive_offers_checkbox).toBeEnabled();
-    await homepage.locators.receive_offers_checkbox.check();
-    await expect(homepage.locators.receive_offers_checkbox).toBeChecked();
-    await homepage.locators.receive_offers_checkbox.uncheck();
     await expect(homepage.locators.receive_offers_checkbox).not.toBeChecked();
-
-    await expect(homepage.locators.address_information_heading).toBeVisible();
-
-    await expect(homepage.locators.first_name_field).toBeVisible();
-
-    await expect(homepage.locators.first_name_field).toBeEnabled();
-    await homepage.locators.first_name_field.fill("Automation");
     await expect(homepage.locators.first_name_field).toHaveValue("Automation");
-
-    await expect(homepage.locators.last_name_field).toBeVisible();
-    await expect(homepage.locators.last_name_field).toBeEnabled();
-    await homepage.locators.last_name_field.fill("Automation");
     await expect(homepage.locators.last_name_field).toHaveValue("Automation");
-
-    await expect(homepage.locators.company_field).toBeVisible();
-    await expect(homepage.locators.company_field).toBeEnabled();
-    await homepage.locators.company_field.fill("Automation");
     await expect(homepage.locators.company_field).toHaveValue("Automation");
-
-    await expect(homepage.locators.address1_field).toBeVisible();
-    await expect(homepage.locators.address1_field).toBeEnabled();
-    await homepage.locators.address1_field.fill(address_data);
     await expect(homepage.locators.address1_field).toHaveValue(address_data);
-
-    await expect(homepage.locators.address2_field).toBeVisible();
-    await expect(homepage.locators.address2_field).toBeEnabled();
-    await homepage.locators.address2_field.fill(address_data);
     await expect(homepage.locators.address2_field).toHaveValue(address_data);
-
-    await expect(homepage.locators.country_dropdown).toBeVisible();
-    await expect(homepage.locators.country_dropdown).toBeEnabled();
-    await expect(homepage.locators.country_dropdown).toHaveValue("India");
-    await homepage.locators.country_dropdown.selectOption("United States");
     await expect(homepage.locators.country_dropdown).toHaveValue(
       "United States"
     );
-
-    await expect(homepage.locators.state_field).toBeVisible();
-    await expect(homepage.locators.state_field).toBeEnabled();
-    await homepage.locators.state_field.fill("California");
     await expect(homepage.locators.state_field).toHaveValue("California");
-
-    await expect(homepage.locators.city_field).toBeVisible();
-    await expect(homepage.locators.city_field).toBeEnabled();
-    await homepage.locators.city_field.fill("Los Angeles");
     await expect(homepage.locators.city_field).toHaveValue("Los Angeles");
-
-    await expect(homepage.locators.zipcode_field).toBeVisible();
-    await expect(homepage.locators.zipcode_field).toBeEnabled();
-    await homepage.locators.zipcode_field.fill("90001");
     await expect(homepage.locators.zipcode_field).toHaveValue("90001");
-
-    await expect(homepage.locators.mobile_number_field).toBeVisible();
-    await expect(homepage.locators.mobile_number_field).toBeEnabled();
-    await homepage.locators.mobile_number_field.fill("1234567890");
     await expect(homepage.locators.mobile_number_field).toHaveValue(
       "1234567890"
     );
 
-    await expect(homepage.locators.create_account_button).toBeVisible();
-    await expect(homepage.locators.create_account_button).toBeEnabled();
-    await homepage.locators.create_account_button.click();
-
+    await homepage.submitRegistrationForm();
     console.log("Account creation successful");
 
     // Log Email and password details
     console.log("Email:", email_value);
     console.log("Password:", random_password);
 
+    // Assertions for Account Created page
     await expect(homepage.locators.account_created_heading).toBeVisible();
     await expect(homepage.locators.continue_button).toBeVisible();
-    await homepage.locators.continue_button.click();
+    await homepage.continueToHomepage();
 
-    await expect(homepage.locators.delete_account_button).toBeVisible();
-    await expect(homepage.locators.delete_account_button).toBeEnabled();
-    await homepage.locators.delete_account_button.click();
-
-    await expect(homepage.locators.continue_button).toBeVisible();
+    // Assertions for Delete Account
+    await homepage.deleteAccount();
     await expect(homepage.locators.account_deleted_heading).toBeVisible();
     await expect(homepage.locators.account_deleted_info).toBeVisible();
 
-    await homepage.locators.continue_button.click();
+    await homepage.continueToHomepage();
     console.log("Account deletion successful");
   });
 
-  test("Test Case 2: Login User with correct email and password", async ({
-    page,
-  }) => {});
+  // test("Test Case 2: Login User with correct email and password", async ({
+  //   page,
+  // }) => {});
 });
